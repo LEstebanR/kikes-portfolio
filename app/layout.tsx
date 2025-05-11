dcd823b3ab69e35ac22f17d787e2f7065d7b361d
Luis Esteban Ramírez <leramirezca@gmail.com>
2025-05-10 21:58:27 -0500

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,8 @@ const geistSans = Geist({
   subsets: ["latin"],
 });
 export const metadata: Metadata = {
-  title: "Shadcn UI Portfolio",
-  description:
-    "A beautiful portfolio template built with Shadcn UI, Tailwind CSS 4, and Next.js 15",
+  title: "Luis Villada Portfolio",
+  description: "Portfolio de Luis Villada",
 };
 
 export default function RootLayout({
