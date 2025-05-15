0f33436a6e95fc9d571254a04ba8dd8dd7fd45a7
Luis Esteban Ramírez <leramirezca@gmail.com>
2025-05-14 21:20:35 -0500

    add links

--- components/navbar/nav-menu.tsx
+++ components/navbar/nav-menu.tsx
@@ -8,7 +8,11 @@ import { cn } from "@/lib/utils";
 import { NavigationMenuProps } from "@radix-ui/react-navigation-menu";
 import Link from "next/link";
 
-export const NavMenu = ({ className, ...props }: NavigationMenuProps) => (
+interface NavMenuProps extends NavigationMenuProps {
+  onLinkClick?: () => void;
+}
+
+export const NavMenu = ({ className, onLinkClick, ...props }: NavMenuProps) => (
   <NavigationMenu
     className={cn("data-[orientation=vertical]:items-start", className)}
     {...props}
@@ -16,22 +20,30 @@ export const NavMenu = ({ className, ...props }: NavigationMenuProps) => (
     <NavigationMenuList className="gap-1 space-x-0 data-[orientation=vertical]:flex-col data-[orientation=vertical]:items-start">
       <NavigationMenuItem>
         <NavigationMenuLink asChild>
-          <Link href="#about">Sobre mí</Link>
+          <Link href="#about" onClick={onLinkClick}>
+            Sobre mí
+          </Link>
         </NavigationMenuLink>
       </NavigationMenuItem>
       <NavigationMenuItem>
         <NavigationMenuLink asChild>
-          <Link href="#experience">Experiencia</Link>
+          <Link href="#experience" onClick={onLinkClick}>
+            Experiencia
+          </Link>
         </NavigationMenuLink>
       </NavigationMenuItem>
       <NavigationMenuItem>
         <NavigationMenuLink asChild>
-          <Link href="#projects">Proyectos</Link>
+          <Link href="#projects" onClick={onLinkClick}>
+            Proyectos
+          </Link>
         </NavigationMenuLink>
       </NavigationMenuItem>
       <NavigationMenuItem>
         <NavigationMenuLink asChild>
-          <Link href="#education">Educación</Link>
+          <Link href="#education" onClick={onLinkClick}>
+            Educación
+          </Link>
         </NavigationMenuLink>
       </NavigationMenuItem>
     </NavigationMenuList>
--- components/navbar/navigation-sheet.tsx
+++ components/navbar/navigation-sheet.tsx
@@ -1,12 +1,16 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Menu } from "lucide-react";
 import { Logo } from "./logo";
 import { NavMenu } from "./nav-menu";
+import { useState } from "react";
 
 export const NavigationSheet = () => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <Sheet>
+    <Sheet open={open} onOpenChange={setOpen}>
       <SheetTrigger asChild>
         <Button variant="outline" size="icon" className="rounded-full">
           <Menu />
@@ -14,7 +18,11 @@ export const NavigationSheet = () => {
       </SheetTrigger>
       <SheetContent className="pt-3 px-6">
         <Logo width={80} height={30} />
-        <NavMenu orientation="vertical" className="mt-12" />
+        <NavMenu
+          orientation="vertical"
+          className="mt-12"
+          onLinkClick={() => setOpen(false)}
+        />
       </SheetContent>
     </Sheet>
   );
